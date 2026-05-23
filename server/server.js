4526702806db9e6b4f7e0d702cf29fa72ce21b7e
Balavignesh J <balav3230@gmail.com>
2026-05-23 14:28:01 +0530

    feat: initialize express server with cors, database connection, and inngest endpoint

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import ConnectDB from "./Configs/db.js";
 import "dotenv/config";
-import { inngest,functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
@@ -15,7 +16,7 @@ const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use("/api/inngest",serve({ client: inngest, functions }))
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
